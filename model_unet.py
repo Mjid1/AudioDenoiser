fbe3b1aafb91130d0de7dcf8e3e88efb7493f79d
WISSALELAMRAOUI <148660134+WISSALELAMRAOUI@users.noreply.github.com>
2025-06-14 19:18:07 +0200

    unet++

--- model_unet.py
+++ model_unet.py
@@ -1,89 +1,61 @@
-
 import numpy as np
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, LeakyReLU, MaxPooling2D, Dropout, concatenate, UpSampling2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import backend
 import tensorflow as tf
-print(tf.__version__)
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (
+    Input, Conv2D, LeakyReLU, MaxPooling2D, Dropout,
+    concatenate, UpSampling2D
+)
+from tensorflow.keras.optimizers import Adam
 
-#Unet network
-def unet(pretrained_weights = None,input_size = (128,128,1)):
-    #size filter input
-    size_filter_in = 16
-    #normal initialization of weights
-    kernel_init = 'he_normal'
-    #To apply leaky relu after the conv layer 
-    activation_layer = None
+print("TensorFlow version:", tf.__version__)
+
+# Bloc de convolution avec activation et dropout 
+def conv_block(x, filters, dropout_rate=0.0):
+    x = Conv2D(filters, 3, padding='same', kernel_initializer='he_normal')(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(filters, 3, padding='same', kernel_initializer='he_normal')(x)
+    x = LeakyReLU()(x)
+    if dropout_rate > 0.0:
+        x = Dropout(dropout_rate)(x)
+    return x
+
+# Modèle U-Net++ (Nested U-Net) avec option poids pré-entraînés
+def unet_plus_plus(input_size=(128, 128, 1), pretrained_weights=None):
     inputs = Input(input_size)
-    conv1 = Conv2D(size_filter_in, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(inputs)
-    conv1 = LeakyReLU()(conv1)
-    conv1 = Conv2D(size_filter_in, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv1)
-    conv1 = LeakyReLU()(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(size_filter_in*2, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(pool1)
-    conv2 = LeakyReLU()(conv2)
-    conv2 = Conv2D(size_filter_in*2, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv2)
-    conv2 = LeakyReLU()(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(size_filter_in*4, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(pool2)
-    conv3 = LeakyReLU()(conv3)
-    conv3 = Conv2D(size_filter_in*4, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv3)
-    conv3 = LeakyReLU()(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(size_filter_in*8, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(pool3)
-    conv4 = LeakyReLU()(conv4)
-    conv4 = Conv2D(size_filter_in*8, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv4)
-    conv4 = LeakyReLU()(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-    conv5 = Conv2D(size_filter_in*16, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(pool4)
-    conv5 = LeakyReLU()(conv5)
-    conv5 = Conv2D(size_filter_in*16, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv5)
-    conv5 = LeakyReLU()(conv5)
-    drop5 = Dropout(0.5)(conv5)
+    # Niveau 0
+    x00 = conv_block(inputs, 16)
+    x10 = conv_block(MaxPooling2D()(x00), 32)
+    x20 = conv_block(MaxPooling2D()(x10), 64)
+    x30 = conv_block(MaxPooling2D()(x20), 128)
+    x40 = conv_block(MaxPooling2D()(x30), 256, dropout_rate=0.5)
 
-    up6 = Conv2D(size_filter_in*8, 2, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(UpSampling2D(size = (2,2))(drop5))
-    up6 = LeakyReLU()(up6)
-    merge6 = concatenate([drop4,up6], axis = 3)
-    conv6 = Conv2D(size_filter_in*8, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(merge6)
-    conv6 = LeakyReLU()(conv6)
-    conv6 = Conv2D(size_filter_in*8, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv6)
-    conv6 = LeakyReLU()(conv6)
-    up7 = Conv2D(size_filter_in*4, 2, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(UpSampling2D(size = (2,2))(conv6))
-    up7 = LeakyReLU()(up7)
-    merge7 = concatenate([conv3,up7], axis = 3)
-    conv7 = Conv2D(size_filter_in*4, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(merge7)
-    conv7 = LeakyReLU()(conv7)
-    conv7 = Conv2D(size_filter_in*4, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv7)
-    conv7 = LeakyReLU()(conv7)
-    up8 = Conv2D(size_filter_in*2, 2, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(UpSampling2D(size = (2,2))(conv7))
-    up8 = LeakyReLU()(up8)
-    merge8 = concatenate([conv2,up8], axis = 3)
-    conv8 = Conv2D(size_filter_in*2, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(merge8)
-    conv8 = LeakyReLU()(conv8)
-    conv8 = Conv2D(size_filter_in*2, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv8)
-    conv8 = LeakyReLU()(conv8)
+    # Niveau 1
+    x01 = conv_block(concatenate([x00, UpSampling2D()(x10)], axis=3), 16)
+    x11 = conv_block(concatenate([x10, UpSampling2D()(x20)], axis=3), 32)
+    x21 = conv_block(concatenate([x20, UpSampling2D()(x30)], axis=3), 64)
+    x31 = conv_block(concatenate([x30, UpSampling2D()(x40)], axis=3), 128)
 
-    up9 = Conv2D(size_filter_in, 2, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(UpSampling2D(size = (2,2))(conv8))
-    up9 = LeakyReLU()(up9)
-    merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(size_filter_in, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(merge9)
-    conv9 = LeakyReLU()(conv9)
-    conv9 = Conv2D(size_filter_in, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv9)
-    conv9 = LeakyReLU()(conv9)
-    conv9 = Conv2D(2, 3, activation = activation_layer, padding = 'same', kernel_initializer = kernel_init)(conv9)
-    conv9 = LeakyReLU()(conv9)
-    conv10 = Conv2D(1, 1, activation = 'tanh')(conv9)
+    # Niveau 2
+    x02 = conv_block(concatenate([x00, x01, UpSampling2D()(x11)], axis=3), 16)
+    x12 = conv_block(concatenate([x10, x11, UpSampling2D()(x21)], axis=3), 32)
+    x22 = conv_block(concatenate([x20, x21, UpSampling2D()(x31)], axis=3), 64)
 
-    model = Model(inputs,conv10)
+    # Niveau 3
+    x03 = conv_block(concatenate([x00, x01, x02, UpSampling2D()(x12)], axis=3), 16)
+    x13 = conv_block(concatenate([x10, x11, x12, UpSampling2D()(x22)], axis=3), 32)
 
-    model.compile(optimizer = 'adam', loss = tf.keras.losses.Huber(), metrics = ['mae'])
+    # Niveau 4
+    x04 = conv_block(concatenate([x00, x01, x02, x03, UpSampling2D()(x13)], axis=3), 16)
 
-    #model.summary()
+    # Sortie
+    output = Conv2D(1, (1, 1), activation='tanh')(x04)
 
-    if(pretrained_weights):
+    model = Model(inputs=inputs, outputs=output)
+    model.compile(optimizer=Adam(), loss=tf.keras.losses.Huber(), metrics=['mae'])
+
+    if pretrained_weights:
         model.load_weights(pretrained_weights)
 
     return model
+
